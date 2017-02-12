1e829e9b58a61778288143ec855e8603261bab96
e-cloud <saintscott119@gmail.com>
2017-02-12 20:29:20 +0800

    example: add basic usage demo

--- demo-app/app/app.module.ts
+++ demo-app/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NgSplitLayoutModule } from '../../lib';
+import { MapComponent } from './map.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgSplitLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ demo-app/app/map.component.ts
@@ -0,0 +1,11 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'demo-map',
+  template: 'Map'
+})
+export class MapComponent {
+  /*@HostListener('layout-resize') onResize() {
+    console.log('bubbled')
+  }*/
+}
